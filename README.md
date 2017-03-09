7f9d1943784ea844f7d7faf034f920db4296408e
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2017-03-09 20:15:13 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # node-red-auth-contrib-ldap
 LDAP authentication module for Node-RED
+
+## Configration to use LDAP authentication
+
+(1) Add LDAP server information into settings.js file
+- vi ~/.node-red/settings.js
+``
+var config = {
+    url: "ldap://<hostname>:389",
+    dn: "uid=%username%,ou=people,o=example,c=jp"
+};
+var ldap = require("node-red-auth-contrib-ldap");
+ldap.init(config);
+
+module.exports = {
+...
+    adminAuth: ldap,
+...
+}
+```
+
+(2) Install LDAP authentication module
+- cp ~/.node-red/node_modules/
+- npm install node-red-auth-contrib-ldap
+
+(3) Run Node-RED
+> node-red
---
+++ ldap.js
@@ -0,0 +1,38 @@
+var when = require("when");
+var ldap = require('ldapjs');
+var config;
+
+module.exports = {
+   init: function(_config) {
+       config = _config;
+   },
+   type: "credentials",
+   users: function(username) {
+       return when.promise(function(resolve) {
+           var user = { username: username, permissions: "*" };
+           resolve(user);
+       });
+   },
+   authenticate: function(username,password) {
+       return when.promise(function(resolve) {
+           var client = ldap.createClient({ url: config.url });
+           client.bind(config.dn.replace("%username%", username), password, function(err) {
+               client.unbind();
+               if (err === null) {
+                   console.log('LDAP login success as ' + username);
+                   var user = { username: username, permissions: "*" };
+                   resolve(user);
+               } else {
+                   console.log('LDAP login failure as ' + username + ': ' + err.message);
+                   resolve(null);
+               }
+           });
+       });
+   },
+   default: function() {
+       return when.promise(function(resolve) {
+           resolve(null);
+       });
+   }
+}
+
